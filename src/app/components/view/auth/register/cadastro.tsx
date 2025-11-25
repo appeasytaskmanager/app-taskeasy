8e10c2175cc3e22002d64dccbbddf9d0b5ca8649
gabrielpriegoo <gabrielvpriego@gmail.com>
2025-11-24 21:38:27 -0300

    refactor: update Cadastro component to use async registration and improve form handling

--- src/app/components/view/auth/register/cadastro.tsx
+++ src/app/components/view/auth/register/cadastro.tsx
@@ -4,26 +4,20 @@ import { Button } from "../../../ui/Button";
 import Link from "next/link";
 
 export function Cadastro() {
-  const [nome, setNome] = useState("");
+  const [name, setNome] = useState("");
   const [email, setEmail] = useState("");
-  const [senha, setSenha] = useState("");
-  const [confirmarSenha, setConfirmarSenha] = useState("");
+  const [password, setPassword] = useState("");
   const [erro, setErro] = useState("");
 
-  function handleCadastro() {
-    if (!nome || !email || !senha || !confirmarSenha) {
-      setErro("Preencha todos os campos.");
-      return;
-    }
-
-    if (senha !== confirmarSenha) {
-      setErro("As senhas não coincidem.");
-      return;
-    }
-
-    setErro("");
-    alert(`Cadastro realizado com: ${nome} (${email})`);
-  }
+  const handleCadastro = async () => {
+    const response = JSON.stringify({ name, email, password });
+    const responseApi = await fetch("/api/auth/register", {
+      method: "POST",
+      body: response,
+    });
+    const data = await responseApi.json();
+    console.log(data);
+  };
 
   return (
     <div className="flex flex-col gap-6 p-8 bg-white rounded-2xl shadow-xl w-96 dark:bg-zinc-900 border border-zinc-200 dark:border-zinc-700">
@@ -32,47 +26,61 @@ export function Cadastro() {
       </h2>
 
       <div className="flex flex-col gap-3">
-        <input
-          type="text"
-          placeholder="Nome"
-          value={nome}
-          onChange={(e) => setNome(e.target.value)}
-          className="px-4 py-3 border rounded-lg text-sm text-zinc-800 dark:text-zinc-100 dark:bg-zinc-800 border-zinc-300 dark:border-zinc-700 focus:ring-2 ring-blue-500 outline-none"
-        />
+        <form onSubmit={handleCadastro}>
+          <input
+            type="text"
+            placeholder="Nome"
+            value={name}
+            onChange={(e) => setNome(e.target.value)}
+            className="px-4 py-3 border rounded-lg text-sm text-zinc-800 dark:text-zinc-100 dark:bg-zinc-800 border-zinc-300 dark:border-zinc-700 focus:ring-2 ring-blue-500 outline-none"
+          />
 
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          className="px-4 py-3 border rounded-lg text-sm text-zinc-800 dark:text-zinc-100 dark:bg-zinc-800 border-zinc-300 dark:border-zinc-700 focus:ring-2 ring-blue-500 outline-none"
-        />
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="px-4 py-3 border rounded-lg text-sm text-zinc-800 dark:text-zinc-100 dark:bg-zinc-800 border-zinc-300 dark:border-zinc-700 focus:ring-2 ring-blue-500 outline-none"
+          />
 
-        <input
-          type="password"
-          placeholder="Senha"
-          value={senha}
-          onChange={(e) => setSenha(e.target.value)}
-          className="px-4 py-3 border rounded-lg text-sm text-zinc-800 dark:text-zinc-100 dark:bg-zinc-800 border-zinc-300 dark:border-zinc-700 focus:ring-2 ring-blue-500 outline-none"
-        />
+          <input
+            type="password"
+            placeholder="Senha"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="px-4 py-3 border rounded-lg text-sm text-zinc-800 dark:text-zinc-100 dark:bg-zinc-800 border-zinc-300 dark:border-zinc-700 focus:ring-2 ring-blue-500 outline-none"
+          />
 
-        <input
-          type="password"
-          placeholder="Confirmar Senha"
-          value={confirmarSenha}
-          onChange={(e) => setConfirmarSenha(e.target.value)}
-          className="px-4 py-3 border rounded-lg text-sm text-zinc-800 dark:text-zinc-100 dark:bg-zinc-800 border-zinc-300 dark:border-zinc-700 focus:ring-2 ring-blue-500 outline-none"
-        />
+          {/* <input
+            type="password"
+            placeholder="Confirmar Senha"
+            value={confirmarSenha}
+            onChange={(e) => setConfirmarSenha(e.target.value)}
+            className="px-4 py-3 border rounded-lg text-sm text-zinc-800 dark:text-zinc-100 dark:bg-zinc-800 border-zinc-300 dark:border-zinc-700 focus:ring-2 ring-blue-500 outline-none"
+          /> */}
+        </form>
+        <button
+          className="w-full py-2 bg-blue-600 hover:bg-blue-700 text-white rounded-md transition"
+          // type="submit"
+          // onClick={handleCadastro}
+          onClick={() => handleCadastro()}
+        >
+          Cadastrar
+        </button>
       </div>
 
       {erro && <p className="text-sm text-red-500 text-center">{erro}</p>}
 
-      <Button title="Cadastrar" action={handleCadastro} />
+      {/* <Button title="Cadastrar" action={handleCadastro} /> */}
 
       <p className="text-sm text-center text-zinc-700 dark:text-zinc-300">
         Já tem conta?{" "}
-
-        <Link href="/auth/login" className="text-blue-600 dark:text-blue-400 hover:underline">Entrar</Link>
+        <Link
+          href="/auth/login"
+          className="text-blue-600 dark:text-blue-400 hover:underline"
+        >
+          Entrar
+        </Link>
       </p>
     </div>
   );
