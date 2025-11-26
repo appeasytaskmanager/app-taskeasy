9c7401b0c2b276808cd0663014114057fd3c4151
Ruanprogramas <ruandemouratrindade@gmail.com>
2025-11-26 20:39:20 -0300

    criação de dashboard

---
+++ src/app/(pages)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-zinc-100 dark:bg-linear-to-b from-zinc-900 to-zinc-800">
+      <div className="flex items-center justify-center p-4 md:p-8">
+        dashboard
+      </div>
+    </div>
+  );
+}
